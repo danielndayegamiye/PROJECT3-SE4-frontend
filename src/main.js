76d3bfe9c56691bbb368a38cf56c87af7947f8b7
Israel <israrelo@gmail.com>
2024-10-21 21:18:25 -0500

    Added utils and store

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import store from './store/store'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
+app.use(vuetify)
 
 app.mount('#app')

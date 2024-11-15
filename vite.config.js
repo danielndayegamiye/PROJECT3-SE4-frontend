bfebe1ab47638ce1cdb78c8537c450add3fb76fe
Daniel Ndayegamiye <d.ndayegamiye@eagles.oc.edu>
2024-11-15 10:25:28 -0600

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/2024/project3/t7/',
   server: {
     port: 8081,
   },
